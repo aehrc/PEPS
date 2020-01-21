c1d1e7cc7f7e8eb31f1003601b667823598d704e
ArashBayatDev <Arash.Bayat@csiro.au>
2020-01-20 18:11:55 -0800

    fix convertor; fix Probability formula

--- VCF_2_CSV.sh
+++ VCF_2_CSV.sh
@@ -21,7 +21,7 @@ echo " ***** This scripts does not check input arguments."
 echo " >>>>> Converting...."
 
 bcftools query -f '%CHROM:%POS:%REF:%ALT\n' $VcfFile > $VcfFile.id.tsv
-bcftools query -l  small.vcf | awk 'BEGIN{print("SNP")}{print}' | tr \\n , | sed 's/.$//g' | awk '{print}' > $VcfFile.head
+bcftools query -l  $VcfFile | awk 'BEGIN{print("SNP")}{print}' | tr \\n , | sed 's/.$//g' | awk '{print}' > $VcfFile.head
 bcftools query -f '[%GT\t]\n' $VcfFile | tr -d '|' | tr -d '/' | tr '\.' '0' | sed 's/00/0/g' | sed 's/01/1/g' | sed 's/10/1/g' | sed 's/11/2/g' | paste $VcfFile.id.tsv - | tr \\t ',' | sed 's/.$//g' | cat $VcfFile.head - > $VcfFile.csv
 
 echo " >>>>> Done"
